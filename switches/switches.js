27351b1ec4269e1a1764104628fd77323935a85b
gabastil <glenn.abastillas@gmail.com>
2020-05-23 21:44:54 -0400

    Finished basic switches study

--- switches/switches.js
+++ switches/switches.js
@@ -1,4 +1,33 @@
 $(document).ready(() => {
-    const basic = $("div[class='switch']"),
-          solid = $("div[class='switch' & class*='primary']");
+    const switch_ = $("div[class*='switch']");
+
+    switch_.click(function(){
+        const current = $(this);
+              knob = current.find('.knob'),
+              track = current.find('.track'),
+              is_primary = current.is("[class*='primary']"),
+              is_secondary = current.is("[class*='secondary']");
+
+        console.log(is_primary);
+        console.log(is_secondary);
+        console.log(track);
+
+        if (knob.hasClass('switched-on')){
+            knob.removeClass('switched-on');
+
+            setTimeout(() => {
+                if (is_primary || is_secondary) {
+                    track.removeClass('track-on');
+                }
+            }, 250);
+        } else {
+            knob.addClass('switched-on');
+
+            setTimeout(() => {
+                if (is_primary || is_secondary) {
+                    track.addClass('track-on');
+                }
+            }, 250);
+        }
+    });
 });
